5612a770881e2e1c08ef559ae9e85698854233ef
Pawel Karauda <pawelkarauda@bitbucket.com>
2019-03-12 18:47:53 +0100

    Done

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "mobx-react";
+import { stores } from "./stores";
+
+ReactDOM.render(
+  <Provider {...stores}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
